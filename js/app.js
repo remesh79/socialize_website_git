15e9aeaf7504041b47b27d24073b8f91b2140c99
kenjilabs <uzz.art@gmail.com>
2015-02-05 10:09:09 +0400

    updates

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+function isTouch(){
+    return Modernizr.touch;
+}
 var App = {
     init:function(){
         var self = this;
@@ -5,12 +8,24 @@ var App = {
         $(window).resize(function () {
             self.resize()
         });
+
+        // mobile nav button
+        $('.header-nav').on('click', function (e) {
+            e.preventDefault();
+            $('body').toggleClass('nav-open');
+        })
+
     }, resize: function(){
         var h = $(window).height() - $('#header').height();
-        $("#home").css({
-            minHeight: h
-        });
+
+        if(!isTouch()){
+            $("#home").css({
+                minHeight: h
+            });
+        }
+
     }
+
 }
 
 $(function(){
