12f787d0cb6754995b7c871821ecfa60e4738e66
kenjilabs <uzz.art@gmail.com>
2015-02-05 15:49:29 +0400

    updates

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 function isTouch(){
     return Modernizr.touch;
 }
+
+function createSlider(data){
+    var content = "";
+    for(var i in data["items"]){
+        var img = data["items"][i].img;
+        var alt = data["items"][i].alt;
+
+        content += "<img src=\"" +img+ "\" alt=\"" +alt+ "\">"
+    }
+    $('#case_studies_slider').html(content);
+}
 var App = {
     init:function(){
         var self = this;
@@ -14,6 +25,14 @@ var App = {
             e.preventDefault();
             $('body').toggleClass('nav-open');
         })
+
+        /* case study slider*/
+        $("#case_studies_slider").owlCarousel({
+            singleItem : true,
+            autoHeight : true,
+            jsonPath : 'json/case_studies_slider.json',
+            jsonSuccess : createSlider
+        });
 
     }, resize: function(){
         var h = $(window).height() - $('#header').height();
